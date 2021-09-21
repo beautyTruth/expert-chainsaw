41944c62f6106a39f80fd15c1f01368305a88351
Phil <happy.melodist@gmail.com>
2021-09-21 10:34:57 -0500

    complete javascript controls for video which thusly completes section 2

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("change");
 });
 // end of navbar
+
+// section 2 (video controls)
+const videoSource = document.querySelector(".video");
+const videoBtn = document.querySelector(".buttons i");
+const videoBar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (videoSource.paused) {
+    videoSource.play();
+    videoBtn.className = "far fa-pause-circle";
+    videoSource.style.opacity = ".7";
+  } else {
+    videoSource.pause();
+    videoBtn.className = "far fa-play-circle";
+    videoSource.style.opacity = ".3";
+  }
+};
+
+videoBtn.addEventListener("click", () => {
+  playPause();
+});
+
+videoSource.addEventListener("timeupdate", () => {
+  const barWidth = videoSource.currentTime / videoSource.duration;
+  videoBar.style.width = `${barWidth * 100}%`;
+  if (videoSource.ended) {
+    videoBtn.className = "far fa-play-circle";
+    videoSource.style.opacity = ".3";
+  }
+});
+
+// end of section 2 (video controls)
